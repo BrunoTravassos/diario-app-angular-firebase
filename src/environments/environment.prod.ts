ca7c5d5b5bfb4ca8ed841fbd67628ddb5d95b815
BrunoTravassos <brunodefrontin@gmail.com>
2021-10-18 13:35:37 -0300

    first commit projeto diario-app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAVC2vu_PoadXmwbtIX4hars6XfDogrz_E",
+    authDomain: "diario-angular-1-7c537.firebaseapp.com",
+    projectId: "diario-angular-1-7c537",
+    storageBucket: "diario-angular-1-7c537.appspot.com",
+    messagingSenderId: "824871880582",
+    appId: "1:824871880582:web:a3775a6ab47056bafa8e07",
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAVC2vu_PoadXmwbtIX4hars6XfDogrz_E",
+    authDomain: "diario-angular-1-7c537.firebaseapp.com",
+    projectId: "diario-angular-1-7c537",
+    storageBucket: "diario-angular-1-7c537.appspot.com",
+    messagingSenderId: "824871880582",
+    appId: "1:824871880582:web:a3775a6ab47056bafa8e07",
+  },
 };
 
 /*
